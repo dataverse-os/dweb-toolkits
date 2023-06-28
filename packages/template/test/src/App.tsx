07dd5d6a4d6e0b6714581a858e28c651bb701809
Canvas Lee <746591811@qq.com>
2023-06-28 17:10:52 +0800

    feat: add template package

---
+++ packages/template/test/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import { useContext, useEffect, useMemo, useState } from "react";
+import { ethers } from "ethers";
+
+const App = () => {
+  
+  return (
+    <div id="App">
+
+    </div>
+  );
+};
+
+export default App;
