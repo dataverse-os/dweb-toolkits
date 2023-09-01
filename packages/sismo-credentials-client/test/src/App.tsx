37690c8d53a3f6116ed15140d48ae00103700d6d
Canvas Lee <746591811@qq.com>
2023-09-01 14:52:26 +0800

    feat: init sismo-credentials-client

---
+++ packages/sismo-credentials-client/test/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import { useContext, useEffect, useMemo, useState } from "react";
+
+const App = () => {
+  
+  return (
+    <div id="App">
+
+    </div>
+  );
+};
+
+export default App;
